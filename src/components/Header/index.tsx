bb3ed7a834a15cd9bb758011f94b3d6ca9b70564
ImxYJL <allensain14@gmail.com>
2025-01-10 22:33:53 +0900

    feat: 헤더에 모달 핸들러 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
 import * as S from "./styles";
 import addButtonSrc from "../../assets/add-button.png";
 
-const Header = () => {
+interface HeaderProps {
+  openAddModal: () => void;
+}
+
+const Header = ({ openAddModal }: HeaderProps) => {
   return (
     <S.Gnb>
       <S.HeaderText>점심 뭐 먹지</S.HeaderText>
-      <S.RestaurantAddButton type="button" aria-label="음식점 추가">
+      <S.RestaurantAddButton
+        type="button"
+        aria-label="음식점 추가"
+        onClick={openAddModal}
+      >
         <img src={addButtonSrc} alt="" />
       </S.RestaurantAddButton>
     </S.Gnb>
